9d37b91201ca858180fa8f68da2072c5d0cedf9e
Abbas2001Dossa <k201660@nu.edu.pk>
2023-02-19 08:49:40 -0800

    end of lec44 -- routes and schemas done

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,10 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello my name is ABbas Dossa !')
-})
+// Availalble Routes 
+// professionally handling hte routes ! 
+app.use('/api/auth' , require('./routes/auth'));
+app.use('/api/notes' , require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// this is for the only dash  after /api/auth/    ---- thus like this we can define more rouytes which could directly access 
+router.get('/' , (req,res)=>{
+    
+    // making a js object we can send in response ! 
+    obj = {
+        name : "Abbas Dossa",
+        age : 21
+    }
+
+    // sending response in json format ! 
+    res.json(obj);
+});
+
+
+
+// necessary to make it work ! and for a complete connection so the response go back as welll! 
+
+module.exports = router
